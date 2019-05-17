b7a0e8712f91289269d177dfa99862fe8806e35e
Matheus Nicolielo <matheusnicolielo@gmail.com>
2019-05-17 16:16:26 -0300

    Arrumando nome packg

--- src/aep/App.java
+++ src/aep/App.java
@@ -4,8 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		Juridica carlos = new Juridica("Carlos","123",1);
 		//System.out.println(carlos.getCnpj());
-		carlos.adcionarSocio(carlos,456.00);
+		carlos.adcionarSocio(carlos,45.00);
+		carlos.adcionarSocio(carlos,70.00);
 		carlos.listarSocios();
-		
 	}
 }
